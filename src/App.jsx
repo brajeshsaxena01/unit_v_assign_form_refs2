c84e9baffefa7fb533427d0376f45776ec0edc2e
Brajesh Kumar Saxena <95098581+brajeshsaxena01@users.noreply.github.com>
2022-04-21 22:58:08 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+import { Form } from "./components/Form";
+import { Table } from "./components/Table";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Form />
+      <Table />
+    </div>
+  );
+}
